eaa8de6f6902138d94c0fac490ba4ef94a3bb170
David Sosa <david.sosa@protonmail.com>
2020-10-04 18:12:44 +0200

    Wrote assembly for loop and multiplication

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+
+@R0
+D = M
+
+@n
+M = D
+
+@i
+M = D
+
+(LOOP)
+@i
+D = M
+@STOP
+D; JEQ
+
+@i
+M = D - 1
+@LOOP
+0;JMP
+
+(STOP)
+
+
+(END)
+@END
+0;JMP
