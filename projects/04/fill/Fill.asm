ba3b4ced965f21191f77c835c9e8c16e05e920d0
David Sosa <david.sosa@protonmail.com>
2020-10-18 19:32:21 +0200

    Finished up Fill.asm project

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,12 +11,54 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
-
+// screen: 16384 + 8191
+// Listening to keyboard input
 (LOOP)
+@i
+M=0
 @24576
 D = M
+@BLACKLOOP
+D;JNE
+@BLANKLOOP
+D;JEQ
 
+(BLACKLOOP)
+@i
+D=M
+@8191
+D=D-A
+@LOOP
+D;JGT // If (i-8191)=0 goto END
+@SCREEN
+D = A
+@i
+A = D+M
+M = -1
+@i
+M = M+1
+@BLACKLOOP
+0;JMP
+
+(BLANKLOOP)
+@i
+D=M
+@8191
+D=D-A
+@LOOP
+D;JGT // If (i-8191)=0 goto END
+@SCREEN
+D = A
+@i
+A = D+M
+M = 0
+@i
+M = M+1
+@BLANKLOOP
+0;JMP
+
+@24576 // Reset keyboard value
+M = 0
 
 @LOOP
 0;JMP
